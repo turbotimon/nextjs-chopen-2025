6cd136630331a49b05d26c56cbe11ba4066fae0f
Jonas Bandi <dev@jonasbandi.net>
2025-09-07 21:15:14 +0200

    Drizzle Demo

--- 99-playground/drizzle-setup/src/index.ts
+++ 99-playground/drizzle-setup/src/index.ts
@@ -7,12 +7,18 @@ import {DB_FILE} from '../drizzle.config';
 const db = drizzle(DB_FILE);
 
 async function main() {
-  const firstToDo: typeof todos.$inferInsert = {
+  const newToDos: typeof todos.$inferInsert[] = [{
     title: 'Learn Next.js',
     completed: false,
-  };
+  }, {
+    title: 'Learn React',
+    completed: false,
+  }, {
+    title: 'Learn TypeScript',
+    completed: true,
+  }];
   
-  await db.insert(todos).values(firstToDo);
+  await db.insert(todos).values(newToDos);
   console.log('New todo created!')
   const todoItems = await db.select().from(todos);
   console.log('Getting all todos from the database: ', todoItems)
@@ -22,10 +28,11 @@ async function main() {
     .set({
       completed: true,
     })
-    .where(eq(todos.title, firstToDo.title));
+    .where(eq(todos.title, newToDos[0].title));
   console.log('todo item updated!')
 
-  await db.delete(todos).where(eq(todos.title, firstToDo.title));
+  await db.delete(todos).where(eq(todos.title, newToDos[0].title));
   console.log('todo item deleted!')
+  await db.delete(todos); // delete all
 }
 main();
