376d318050d512ab854c2f477654cf1e4c43b533
Jonas Bandi <dev@jonasbandi.net>
2025-09-07 18:13:09 +0200

    Canary version of Next.js

--- 01-project-starter/next.config.ts
+++ 01-project-starter/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typedRoutes: true,
+  experimental: {
+    cacheComponents: true,
+    staleTimes: {
+      dynamic: 0,
+      static: 0,
+    }
+  },
 };
 
 export default nextConfig;
