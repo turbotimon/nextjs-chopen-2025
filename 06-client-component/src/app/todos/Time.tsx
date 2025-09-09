dc071f864f4a4f5be379c358d38a127a4200e3b7
Timon Erhart <57718207+turbotimon@users.noreply.github.com>
2025-09-09 14:13:27 +0000

    fix hydration error using Time with effects

--- 06-client-component/src/app/todos/Time.tsx
+++ 06-client-component/src/app/todos/Time.tsx
@@ -1,7 +1,19 @@
 'use client'
+
+import { use, useEffect, useState } from "react";
+
+
 export function Time() {
+    // Mast be INSIDE the JSX component
+    // Effects are NOT executed on the server. only the default ("") is used
+    const [time, setTime] = useState("");
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(new Date().toLocaleTimeString());
+        }, 1000);
+    });
     console.log('Time component rendered on client');
     return (
-        <div>Time: {new Date().toLocaleTimeString()}</div>
+        <div>Time: {time}</div>
     );
 }
